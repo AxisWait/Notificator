715d462ab14e13cfc4519aabce13f92119321a7f
AxisWait <axiswait@gmail.com>
2019-08-29 19:28:30 +0300

    Added Toolbar and search icon.

--- app/src/main/java/com/rododendron/notificatorandroid/MainActivity.java
+++ app/src/main/java/com/rododendron/notificatorandroid/MainActivity.java
@@ -1,15 +1,31 @@
 package com.rododendron.notificatorandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+    toolbar =  findViewById(R.id.toolbar);
+    toolbar.setTitle(R.string.app_name);
+    toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            return false;
+        }
+    });
+    toolbar.inflateMenu(R.menu.menu);
     }
 }
